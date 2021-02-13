d28f233a04b7047f9b6246d8f42bba551a1e2cb3
schuhweissnicht <66971051+schuhweissnicht@users.noreply.github.com>
2021-02-13 13:47:26 +0100

    Update simpleLikes.py

--- simpleLikes.py
+++ simpleLikes.py
@@ -12,21 +12,22 @@ user = api.me()
 
 t = random.randint(26, 41)
 
-txt = pyfiglet.figlet_format("Twitter Bot", font="slant")
-
 search = 'YOUR KEYWORD'
 likes = 9
+
+
+txt = pyfiglet.figlet_format(f'{search}', font="slant")
 
 print(txt)
 
 for tweet in tweepy.Cursor(api.search, search).items(likes):
     try:
         tweet.favorite()
-        print(f'likes a tweet from search: {search}')
+        print(f'• likes a tweet from search: {search}')
         time.sleep(t)
     except tweepy.TweepError as e:
         print(e.reason)
-        print(f'failed to like = paused for {t} sekonds')
+        print(f'• failed to like a tweet from search: {search}')
         time.sleep(t)
     except StopIteration:
         break
